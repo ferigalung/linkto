450f365df174d30838f6d0d4965e5e910c8e2d36
ferigalung <ferigalungcom@gmail.com>
2023-01-03 11:27:35 +0700

    finishing

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,10 @@ function App() {
       <div className='container'>
         <ProfilePic />
         <Switch />
-        <Button text='Coba 1' link='https://twitter.com' />
-        <Button text='Coba 2' link='https://twitter.com' />
-        <Button text='Coba 3' link='https://twitter.com' />
-        <Button text='Coba 4' link='https://twitter.com' />
-
+        <Button text='Article - Cara Deploy Dockerized App ke VPS' link='https://www.notion.so/ferigalung/Deploy-Dockerized-App-into-VPS-e65f1463bad744dd9503235e451c4507' />
+        <Button text='Repo - Linkto' link='https://github.com/ferigalung/linkto' />
+        <Button text='Rekomendasi Earphone - Tangzu Wan er' link='https://tokopedia.link/Kp9f8Bjjhwb' />
+        <Button text='Rekomendasi Mic Buat Meeting - Boya M1' link='https://tokopedia.link/CcfZD7njhwb' />
         <Socials />
 
         <Footer />
