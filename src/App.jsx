7e264acf59458323a4c81855b89fa22e9a756479
ferigalung <ferigalungcom@gmail.com>
2023-01-03 12:35:49 +0700

    change link notion

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className='container'>
         <ProfilePic />
         <Switch />
-        <Button text='Article - Cara Deploy Dockerized App ke VPS' link='https://www.notion.so/ferigalung/Deploy-Dockerized-App-into-VPS-e65f1463bad744dd9503235e451c4507' />
+        <Button text='Article - Cara Deploy Dockerized App ke VPS' link='https://ferigalung.notion.site/Deploy-Dockerized-App-into-VPS-e65f1463bad744dd9503235e451c4507' />
         <Button text='Repo - Linkto' link='https://github.com/ferigalung/linkto' />
         <Button text='Rekomendasi Earphone - Tangzu Wan er' link='https://tokopedia.link/Kp9f8Bjjhwb' />
         <Button text='Rekomendasi Mic Buat Meeting - Boya M1' link='https://tokopedia.link/CcfZD7njhwb' />
