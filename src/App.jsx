e6f403dab5383332adc2613c4207d286abada4fb
ferigalung <ferigalungcom@gmail.com>
2023-01-10 10:40:59 +0700

    change aff link and add new repo express boilerplate link

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,11 @@ function App() {
       <div className='container'>
         <ProfilePic />
         <Switch />
+        <Button text='Repo - Express REST Boilerplate' link='https://github.com/ferigalung/express-rest-boiler-plate' />
         <Button text='Article - Cara Deploy Dockerized App ke VPS' link='https://ferigalung.notion.site/Deploy-Dockerized-App-into-VPS-e65f1463bad744dd9503235e451c4507' />
         <Button text='Repo - Linkto' link='https://github.com/ferigalung/linkto' />
-        <Button text='Rekomendasi Earphone - Tangzu Wan er' link='https://tokopedia.link/Kp9f8Bjjhwb' />
-        <Button text='Rekomendasi Mic Buat Meeting - Boya M1' link='https://tokopedia.link/CcfZD7njhwb' />
+        <Button text='Rekomendasi Earphone - Tangzu Wan er' link='https://tokopedia.link/2IDWuHqSswb' />
+        <Button text='Rekomendasi Mic Buat Meeting - Boya M1' link='https://tokopedia.link/Ea6AK0tSswb' />
         <Socials />
 
         <Footer />
